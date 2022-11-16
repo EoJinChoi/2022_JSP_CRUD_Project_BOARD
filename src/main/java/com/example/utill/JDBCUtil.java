8cf647ce15cf8a987abe3aa3dad1f74d1add448d
choieojin <22100742@handong.ac.kr>
2022-11-16 23:10:31 +0900

    Initial commit

---
+++ src/main/java/com/example/utill/JDBCUtil.java
@@ -0,0 +1,25 @@
+package com.example.utill;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class JDBCUtil {
+	public static Connection getConnection(){  
+	    Connection con=null;  
+	    try{  
+	        Class.forName("com.mysql.jdbc.Driver");  
+	        con= DriverManager.getConnection("jdbc:mysql://db4free.net:3306/dbname","p3_22100742","Xieni9tie2");
+	    }catch(Exception e){
+	    	System.out.println(e);
+	    }  
+	    return con;  
+	}  
+	
+	public static void main(String ars[]) {
+		Connection conn = getConnection();
+		if(conn != null)
+			System.out.println("DB 연결됨!");
+		else
+			System.out.println("DB 연결중 오류 !");
+	}
+}
